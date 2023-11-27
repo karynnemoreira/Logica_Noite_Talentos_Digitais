3330943dd26740727127cb5f9a75320bcc9c5b41
karynnemoreira <karynnemoreira@yahoo.com.br>
2023-11-27 20:33:44 -0300

    atualização com a aula do dia 27 de novembro

--- script.js
+++ script.js
@@ -1,148 +1,129 @@
-// //Variável: é uma caixinha/gaveta para guardar informações.
+// // //Variável: é uma caixinha/gaveta para guardar informações.
 
-// //var, é uma variável declarada FORA DE UMA FUNÇÃO. ELA NÃO É MAIS UTILIZADA, por trazer vários problemas com o uso. 
+// // //var, é uma variável declarada FORA DE UMA FUNÇÃO. ELA NÃO É MAIS UTILIZADA, por trazer vários problemas com o uso. 
 
-// // = quer dizer receber
-// // "" quer dizer que é um string (textos, letras..) tudo que tiver dentro de " " vai ser entendido como texto.
+// // // = quer dizer receber
+// // // "" quer dizer que é um string (textos, letras..) tudo que tiver dentro de " " vai ser entendido como texto.
 
-// var cor = "azul";
-// console.log(cor)
+// // var cor = "azul";
+// // console.log(cor)
 
-// cor = "vermelho";
-// console.log(cor)
+// // cor = "vermelho";
+// // console.log(cor)
 
 
-// //let é uma variável que é A MAIS UTILIZADA E PREFERIDA DOS PROGRAMADOS.
+// // //let é uma variável que é A MAIS UTILIZADA E PREFERIDA DOS PROGRAMADOS.
 
-// //let é o tipo da variável
-// //vnwnoite é o nome da variável
-// //melhor turma é o valor que está sendo guardado nessa variável.
+// // //let é o tipo da variável
+// // //vnwnoite é o nome da variável
+// // //melhor turma é o valor que está sendo guardado nessa variável.
 
-// let vnwnoite = "melhor turma";
-// console.log(vnwnoite)
+// // let vnwnoite = "melhor turma";
+// // console.log(vnwnoite)
 
-// //Não posso utilizar duas variáveis com o mesmo nome
-// //let vnwnoite = "Boa noite!"
+// // //Não posso utilizar duas variáveis com o mesmo nome
+// // //let vnwnoite = "Boa noite!"
 
-// let bia = 17
-// let elaine = 19
-// let evelyn = 44
+// // let bia = 17
+// // let elaine = 19
+// // let evelyn = 44
 
-// console.log(bia - elaine)
-// console.log(evelyn - elaine)
+// // console.log(bia - elaine)
+// // console.log(evelyn - elaine)
 
-// //const é uma variável constante, ELA NÃO MUDA. 
+// // //const é uma variável constante, ELA NÃO MUDA. 
 
-// const cpf = 123456700;
+// // const cpf = 123456700;
 
-// console.log(cpf)
+// // console.log(cpf)
 
-// //cpf = 765432100;
-// //console.log(cpf)
-// //não posso reutilizar uma variável que é constante
+// // //cpf = 765432100;
+// // //console.log(cpf)
+// // //não posso reutilizar uma variável que é constante
 
-// const nascimento = 2001
-// console.log(nascimento)
-// // nascimento = 2002
+// // const nascimento = 2001
 // // console.log(nascimento)
+// // // nascimento = 2002
+// // // console.log(nascimento)
 
 
-// let dia = true
+// // let dia = true
 
 
-// console.log(typeof nascimento)
-// console.log(typeof vnwnoite)
-// console.log (typeof dia)
+// // console.log(typeof nascimento)
+// // console.log(typeof vnwnoite)
+// // console.log (typeof dia)
 
-//aula dia 24.11
+// //aula dia 24.11
 
-//Variável é uma caixinha que guardo alguma informação, pode ser textual (string), number (número), boolean (verdade(true) e falso (false))
+// //Variável é uma caixinha que guardo alguma informação, pode ser textual (string), number (número), boolean (verdade(true) e falso (false))
 
-//const é uma variável constante, ELA NÃO MUDA. Não consigo reutilizar essa variável.
+// //const é uma variável constante, ELA NÃO MUDA. Não consigo reutilizar essa variável.
 
-const dispositivo = "celular"; 
-console.log(dispositivo)
-
-// dispositivo = "tablet"
+// const dispositivo = "celular"; 
 // console.log(dispositivo)
 
-const dataNasc = 2005;
-console.log(dataNasc)
+// // dispositivo = "tablet"
+// // console.log(dispositivo)
+
+// const dataNasc = 2005;
+// console.log(dataNasc)
 
 
-//CONSOLE.LOG NÃO TEM O PODER DE SALVAR/GRAVAR/ARMAZENAR NADA.
+// //CONSOLE.LOG NÃO TEM O PODER DE SALVAR/GRAVAR/ARMAZENAR NADA.
 
 
-// //let é o tipo de variável que é MAIS UTILIZADA, consigo reutilizar. 
+// // //let é o tipo de variável que é MAIS UTILIZADA, consigo reutilizar. 
 
-// let cor = "verde"
-// console.log(cor)
+// // let cor = "verde"
+// // console.log(cor)
 
-// cor = "amarelo"
-// console.log(cor)
+// // cor = "amarelo"
+// // console.log(cor)
 
-// //Nomes de variáveis NÃO PODEM começar com números e símbolos, exceto: $ e _
-// let $cor = "vermelho"
-// let _cor = "rosa"
-// let corNova = "roxo"
+// // //Nomes de variáveis NÃO PODEM começar com números e símbolos, exceto: $ e _
+// // let $cor = "vermelho"
+// // let _cor = "rosa"
+// // let corNova = "roxo"
 
-// console.log($cor)
-// console.log(_cor)
-// console.log(corNova)
-
-
-// //Saber o tipo da variável 
-// let legumes = "cenoura"; //guardando string (texto) com aspas duplas ""
-// let _legumes = 'batata'; //guardando string (texto) com aspas simples ''
-// let $legumes = `abóbora`; //guardando string (texto) com aspas crase ``  
-// let preco = 20; //número, não preciso dew "" ' ' ou ``, ele vai sem nada.
-// let sexta = true
+// // console.log($cor)
+// // console.log(_cor)
+// // console.log(corNova)
 
 
-// console.log(typeof legumes)
-// console.log(typeof preco)
-// console.log(typeof sexta)
+// // //Saber o tipo da variável 
+// // let legumes = "cenoura"; //guardando string (texto) com aspas duplas ""
+// // let _legumes = 'batata'; //guardando string (texto) com aspas simples ''
+// // let $legumes = `abóbora`; //guardando string (texto) com aspas crase ``  
+// // let preco = 20; //número, não preciso dew "" ' ' ou ``, ele vai sem nada.
+// // let sexta = true
 
 
-//TemplateString - Forma mais "elegante" de escrever string. 
-
-// `${}` o templateString SEMPRE vai ser usado com crase ``SEMPRE!! 
-
-
-let aluna1 = "Evelyn"; //guardando uma string/texto utilizando "" aspas duplas
-
-let aluna2 = 'Joy'; //guardando uma string/texto utilizando  '' aspas simples
-
-let aluna3 = `Cris`; //guardando uma string/texto utilizando ``crase
-
-let alunas = `${aluna1 } ${aluna2} ${aluna3}`;
+// // console.log(typeof legumes)
+// // console.log(typeof preco)
+// // console.log(typeof sexta)
 
 
-console.log(aluna1)
-console.log(aluna2)
-console.log(aluna3)
-console.log(alunas)
+// //TemplateString - Forma mais "elegante" de escrever string. 
 
-console.log(`Essas são alunas do vnw: ${aluna1} , ${aluna2} e ${aluna3} `)
- 
-
-//Operadores de comparação 
-
-let numero = 2 //tipo number
-
-// = atribuição de tipo/recebe
-// == verificação de valor (Verifica se o 2 é o mesmo)
-//  === verificação de tipo e valor (verifica se é 2 e se o tipo é number)
-
-// != verifica se o valor comparado é diferente (verifica se o 2 é diferente de um outro valor)
-
-// !== verifica se o valor e o tipo comparado é diferente (verifica se o 2 é diferente de um outro valor, e se ele também é diferente de um number)
+// // `${}` o templateString SEMPRE vai ser usado com crase ``SEMPRE!! 
 
 
+// let aluna1 = "Evelyn"; //guardando uma string/texto utilizando "" aspas duplas
+
+// let aluna2 = 'Joy'; //guardando uma string/texto utilizando  '' aspas simples
+
+// let aluna3 = `Cris`; //guardando uma string/texto utilizando ``crase
+
+// let alunas = `${aluna1 } ${aluna2} ${aluna3}`;
 
 
+// console.log(aluna1)
+// console.log(aluna2)
+// console.log(aluna3)
+// console.log(alunas)
 
-
+// console.log(`Essas são alunas do vnw: ${aluna1} , ${aluna2} e ${aluna3} `)
 
 
 
@@ -161,4 +142,110 @@ let numero = 2 //tipo number
 // let msg = "Hello Word"; //string
 // let resp = numero/msg
 // console.log(resp)
+
+
+//Operadores de comparação 
+
+let numero = 2 //tipo number
+console.log(typeof numero)
+
+// = atribuição de tipo/recebe
+console.log(numero)
+
+// == verificação de valor (Verifica se o 2 é o mesmo)
+console.log(numero == 2)// retournou true pq o valor 2 é 2. 
+console.log(numero == 4) //retornou false pq o valor NÃO é 2. 
+
+
+//  === verificação de tipo e valor (verifica se é 2 e se o tipo é number)
+console.log(numero === 2) //retornou true pq o valor e o tipo são iguais
+console.log(numero === "Karynne") //retornou false pq o valor é diferente e o tipo é uma string
+console.log(numero === "2") //retornou false pq o "2" é entendido como uma string por estar dentro de "aspas"
+
+// != verifica se o valor comparado é diferente (verifica se o 2 é diferente de um outro valor)
+
+console.log(numero != 5) //true, pq o valor é diferente do valor 2
+
+console.log(numero != "uva") //true, pq o valor 2 é diferente do valor "uva"
+
+
+// !== verifica se o valor e o tipo comparado é diferente (verifica se o 2 é diferente de um outro valor, e se ele também é diferente de um number)
+
+console.log(numero !== 3) //true, pq o 2 é diferente de 3
+
+console.log(numero !== 2) //é falsa essa informação. Pq 2 é igual a 2.   
+
+
+// 2 é diferente de 2? 
+
+console.log (numero !== "galinha") //true, o valor e o tipo são diferentes
+
+
+// > maior que
+
+let number = 10 //gavetinha ta guardando o valor 10
+
+console.log(number > 3) //true pq number (10) é maior que o valor 3
+
+console.log (number > 10) //false pq o number(10) não é maior que 10. 
+
+// < menor que
+
+console.log(number < 7) //false pq 10 não é menor que 7
+
+// >= Maior ou igual
+console.log (number >= 10) //true pq 10 é igual a 10
+console.log(number >= 5) //true pq 10 é maior que 5
+
+
+// <= Menor ou igual 
+console.log(number <= 4) //false pq 10 não é menor nem igual a 4
+console.log(number <= 10)//true pq 10 é igual a 10, ele não é menor que 10.
+
+
+//let é o tipo da variável
+//animal é o nome dessa variável
+// "galo" é o valor dessa variável   
+let animal = "galo"
+console.log(animal)
+
+animal="coala" //redefinindo o valor da variável
+console.log(animal)
+
+animal="galinha" 
+console.log(animal)
+
+
+let _animal = "coelho"  //outra variável
+let $animal = "macaco" //outra variável
+let animalDomestico = "gato" //outra variável
+
+//IF ( quer dizer = se) Se essa informação for verdadeira, faça isso: 
+
+// if(condição){
+//     console.log("retorno")
+// }
+
+
+
+let nascimento = 18
+
+if(nascimento >= 18){
+console.log("Você pode acessar o zé delivery")
+} else { //senão, caso a resposta for negativa, cai aqui. 
+    console.log("Vai tomar toddynho")
+}
+
+let dia = "calor"
+
+if (dia === "calor"){
+    console.log("Uhull, vou pra praia!!")
+}
+
+console.log(typeof nascimento) //quero saber qual o tipo da variável nascimento
+console.log(typeof dia) //quero saber qual o tipo da variável dia
+
+let segunda = true
+console.log(typeof segunda)
+
 
